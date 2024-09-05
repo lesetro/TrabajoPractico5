6a7ea6cf560cc01fab35f8e78753c6fadf826835
MauriAbaca <maurikd353@gmail.com>
2024-09-05 20:44:45 -0300

    Metodos terminados
    Termine los metodos buscarTelefono, buscarContactos y borrarContactos.

--- src/main/java/Trabajopractico5/MapContacto.java
+++ src/main/java/Trabajopractico5/MapContacto.java
@@ -4,8 +4,14 @@
  */
 package Trabajopractico5;
 
+import java.util.ArrayList;
+import java.util.Map;
+import static java.util.Map.entry;
+import java.util.Set;
 import java.util.TreeMap;
+import java.util.TreeSet;
 import javax.swing.JOptionPane;
+import javax.swing.RowFilter.Entry;
 
 
 public class MapContacto {
@@ -43,15 +49,30 @@ public class MapContacto {
     }
 //C. buscarTeléfono() que en base a un apellido nos devuelve un Set<Long> con los números 
 //de teléfono asociados a dicho apellido.  
-    public void buscarTelefono(Long telefono, Contacto contacto){
+    public Set<Long> buscarTelofono(String apellido){
+      Set<Long> telefono = new TreeSet<>(); 
+      for (Map.Entry<Long,Contacto> entry : guiaTelefonica.entrySet()){
+          if(entry.getValue().getApellido().equalsIgnoreCase(apellido)){
+              telefono.add(entry.getKey());
+      }
+      }
+        return telefono;
+      
+      }
     
-    }
 //D. buscarContactos() que en base a una ciudad nos devuelve un ArrayList con los 
 //Contactos asociados a dicha ciudad.  
-    public void buscarContactoPorCiudad(Long telefono, Contacto contacto){
+    public ArrayList<Contacto>buscaContactos(String ciudad){
+       ArrayList<Contacto> clientesDeCiudad = new ArrayList<>();
+       for(Contacto contacto : guiaTelefonica.values()){
+           if(contacto.getCiudad().equalsIgnoreCase(ciudad)){
+               clientesDeCiudad.add(contacto);
+          }
+       }
+       return clientesDeCiudad;
     }
 //E. borrarContacto() que en base al número de teléfono elimina el contacto del directorio. 
     public void borrarContacto(Long telefono, Contacto contacto){
-    
+        guiaTelefonica.remove(telefono);
     }
 }
